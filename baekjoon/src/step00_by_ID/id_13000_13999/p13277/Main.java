8eebd70a71cbac2df1d503aed1a2bbe156be9b7c
Luverduck <hyekko92@gmail.com>
2025-05-21 11:51:05 +0900

    [13277번] 큰 수 곱셈

---
+++ baekjoon/src/step00_by_ID/id_13000_13999/p13277/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.src.step00_by_ID.id_13000_13999.p13277;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        bw.append(A.multiply(B).toString());
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
