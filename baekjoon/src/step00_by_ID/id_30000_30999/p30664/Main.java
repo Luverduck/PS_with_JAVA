ffde00bccb026f0a9b1cf79b70fcd50b3f4fbc35
Luverduck <hyekko92@gmail.com>
2025-05-26 21:15:17 +0900

    [30664번] Loteria Falha

---
+++ baekjoon/src/step00_by_ID/id_30000_30999/p30664/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.src.step00_by_ID.id_30000_30999.p30664;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input;
+        while(true) {
+            input = br.readLine();
+            if(input.equals("0")) break;
+            BigInteger N = new BigInteger(input);
+            bw.append(N.mod(new BigInteger("42")).equals(BigInteger.ZERO) ? "PREMIADO" : "TENTE NOVAMENTE").append('\n');
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
