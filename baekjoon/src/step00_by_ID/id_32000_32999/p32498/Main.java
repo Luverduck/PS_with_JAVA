d59dac323ce08d39be15b3ecaca018dd66f269eb
Luverduck <hyekko92@gmail.com>
2025-05-27 20:27:29 +0900

    [32498번] Call for Problems

---
+++ baekjoon/src/step00_by_ID/id_32000_32999/p32498/Main.java
@@ -0,0 +1,24 @@
+package baekjoon.src.step00_by_ID.id_32000_32999.p32498;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int res = 0;
+        for(int n = 0; n < N; ++n) {
+            int D = Integer.parseInt(br.readLine());
+            if(D % 2 != 0) res++;
+        }
+        bw.append(String.valueOf(res));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
