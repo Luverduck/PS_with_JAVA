404b1e045a1f545b7b43997866364a023b15e646
Luverduck <hyekko92@gmail.com>
2025-06-09 02:06:09 +0900

    [33161번] 鉛筆 2 (Pencils 2)

---
+++ baekjoon/src/step00_by_ID/id_33000_33999/p33161/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.src.step00_by_ID.id_33000_33999.p33161;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        bw.append(String.valueOf(A / 5));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
