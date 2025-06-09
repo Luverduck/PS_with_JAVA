3fe388d6a288f90de269cafa79e2407153d3fec8
Luverduck <hyekko92@gmail.com>
2025-06-09 23:51:15 +0900

    [31614번] 分 (Minutes)

---
+++ baekjoon/src/step00_by_ID/id_31000_31999/p31614/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.src.step00_by_ID.id_31000_31999.p31614;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int H = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        bw.append(String.valueOf(60 * H + M));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
