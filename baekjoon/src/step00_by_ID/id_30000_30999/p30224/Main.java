be4f97d65dcdaf6faafeab574d417ebb70b8d2de
Luverduck <hyekko92@gmail.com>
2025-06-12 09:24:07 +0900

    [30224번] Lucky 7

---
+++ baekjoon/src/step00_by_ID/id_30000_30999/p30224/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.src.step00_by_ID.id_30000_30999.p30224;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        boolean hasSeven = input.contains("7");
+        boolean isModOfSeven = Integer.parseInt(input) % 7 == 0;
+
+        int flag = 0;
+        if(hasSeven) flag |= 2;
+        if(isModOfSeven) flag |= 1;
+        switch(flag) {
+            case 0: bw.append("0"); break;
+            case 1: bw.append("1"); break;
+            case 2: bw.append("2"); break;
+            case 3: bw.append("3"); break;
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
