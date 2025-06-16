c12c96176f9c2801f35740688f3ace1a1f1c88f5
Luverduck <hyekko92@gmail.com>
2025-06-17 00:06:52 +0900

    [31610번] 飴の袋詰め (Drops Packing)

---
+++ baekjoon/src/step00_by_ID/id_31000_31999/p31610/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.src.step00_by_ID.id_31000_31999.p31610;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        bw.append(String.valueOf(A * B + C));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
