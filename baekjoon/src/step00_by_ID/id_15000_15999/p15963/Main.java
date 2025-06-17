c959c9715c4e51571c6fc2f9f8831aa6247265f1
Luverduck <hyekko92@gmail.com>
2025-06-17 23:32:09 +0900

    [15963번] CASIO

---
+++ baekjoon/src/step00_by_ID/id_15000_15999/p15963/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.src.step00_by_ID.id_15000_15999.p15963;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String N = st.nextToken();
+        String M = st.nextToken();
+        bw.append(N.equals(M) ? "1" : "0");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
