b2bc49bcf8417d32ff33654cc7b87b842e91007d
Luverduck <hyekko92@gmail.com>
2025-06-23 20:37:22 +0900

    [27328번] 三方比較 (Three-Way Comparison)

---
+++ baekjoon/src/step00_by_ID/id_27000_27999/p27328/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.src.step00_by_ID.id_27000_27999.p27328;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        bw.append(A == B ? "0" : (A > B ? "1" : "-1"));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
