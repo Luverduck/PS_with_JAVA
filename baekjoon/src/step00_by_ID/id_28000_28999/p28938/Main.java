b5c8c9a808141ab9fa9a31f292443c2af4c07558
Luverduck <hyekko92@gmail.com>
2025-06-26 02:04:14 +0900

    [28938번] Конвейер

---
+++ baekjoon/src/step00_by_ID/id_28000_28999/p28938/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.src.step00_by_ID.id_28000_28999.p28938;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int res = 0;
+        while(N-- > 0)
+            res += Integer.parseInt(st.nextToken());
+        bw.append(res == 0 ? "Stay" : (res > 0 ? "Right" : "Left"));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
