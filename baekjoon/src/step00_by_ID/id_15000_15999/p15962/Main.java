92934d87c49c46e9ca4caf6deeec35765fcfcc39
Luverduck <hyekko92@gmail.com>
2025-06-27 16:53:14 +0900

    [15962번] 새로운 시작

---
+++ baekjoon/src/step00_by_ID/id_15000_15999/p15962/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.src.step00_by_ID.id_15000_15999.p15962;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.append("파이팅!!");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
