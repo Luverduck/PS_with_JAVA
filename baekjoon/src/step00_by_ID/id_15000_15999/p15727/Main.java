1a56541fcf58f292b377855ccbcac49df458003c
Luverduck <hyekko92@gmail.com>
2025-06-28 19:13:31 +0900

    [15727번] 조별과제를 하려는데 조장이 사라졌다

---
+++ baekjoon/src/step00_by_ID/id_15000_15999/p15727/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.src.step00_by_ID.id_15000_15999.p15727;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int L = Integer.parseInt(br.readLine());
+        int res = L / 5;
+        if (L % 5 != 0) res++;
+        bw.append(String.valueOf(res));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
