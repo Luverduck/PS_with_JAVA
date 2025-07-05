325aeefa71b45d048e9bc64f5a2e602ff56137f7
Luverduck <hyekko92@gmail.com>
2025-07-05 20:33:42 +0900

    [18096번] Арифметическая магия

---
+++ baekjoon/src/step00_by_ID/id_18000_18999/p18096/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.src.step00_by_ID.id_18000_18999.p18096;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.append("1");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
