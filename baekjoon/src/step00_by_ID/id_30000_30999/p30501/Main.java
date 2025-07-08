b13556341d8f1b6cd0d28af14cd59fd2c754a788
Luverduck <hyekko92@gmail.com>
2025-07-08 19:39:56 +0900

    [30501번] 관공... 어찌하여 목만 오셨소...

---
+++ baekjoon/src/step00_by_ID/id_30000_30999/p30501/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.src.step00_by_ID.id_30000_30999.p30501;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        while(N-- > 0) {
+            String name = br.readLine();
+            if (name.indexOf('S') != -1) {
+                bw.append(name);
+                break;
+            }
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
