6f0e190de77e526e4697b99e258803a5d191c071
Luverduck <hyekko92@gmail.com>
2025-07-27 03:11:10 +0900

    [33689번] CPDU

---
+++ baekjoon/src/step00_by_ID/id_33000_33999/p33689/Main.java
@@ -0,0 +1,24 @@
+package baekjoon.src.step00_by_ID.id_33000_33999.p33689;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int res = 0;
+        for (int i = 0; i < N; ++i) {
+            String s = br.readLine().toUpperCase();
+            if (s.charAt(0) == 'C') res++;
+        }
+        bw.append(String.valueOf(res));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
