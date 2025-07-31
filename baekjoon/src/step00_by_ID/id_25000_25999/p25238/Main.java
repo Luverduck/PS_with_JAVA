394498c9d75bf95c29d47338c29dfcf0e99ee5f7
Luverduck <hyekko92@gmail.com>
2025-08-01 02:20:06 +0900

    [25238번] 가희와 방어율 무시

---
+++ baekjoon/src/step00_by_ID/id_25000_25999/p25238/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.src.step00_by_ID.id_25000_25999.p25238;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int res = A * (100 - B) / 100;
+        bw.append(res < 100 ? "1" : "0");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
