c9a35ec860f9c621223ca24e800b766a3a064511
Luverduck <hyekko92@gmail.com>
2025-08-04 21:34:25 +0900

    [6784번] Multiple Choice

---
+++ baekjoon/src/step00_by_ID/id_06000_06999/p06784/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.src.step00_by_ID.id_06000_06999.p06784;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int res = 0;
+        String[] response = new String[N];
+        for (int i = 0; i < N; ++i)
+            response[i] = br.readLine();
+        for (int i = 0; i < N; ++i) {
+            String answer = br.readLine();
+            if (!response[i].equals(answer)) continue;
+            res++;
+        }
+        bw.append(String.valueOf(res));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
