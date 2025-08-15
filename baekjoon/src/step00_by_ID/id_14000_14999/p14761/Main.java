f1bcb396d41f9657dd67a9069fa7fa6e91d3f1b9
Luverduck <hyekko92@gmail.com>
2025-08-16 01:45:12 +0900

    [14761번] FizzBuzz

---
+++ baekjoon/src/step00_by_ID/id_14000_14999/p14761/Main.java
@@ -0,0 +1,32 @@
+package baekjoon.src.step00_by_ID.id_14000_14999.p14761;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int X = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        for (int n = 1; n <= N; ++n) {
+            if (n % X != 0 && n % Y != 0)
+                sb.append(n);
+            else {
+                if (n % X == 0) sb.append("Fizz");
+                if (n % Y == 0) sb.append("Buzz");
+            }
+            sb.append('\n');
+        }
+        bw.append(sb);
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
