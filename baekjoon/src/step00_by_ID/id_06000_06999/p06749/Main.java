0588a94742a204c68ca28fe3ec534417a18cf43f
Luverduck <hyekko92@gmail.com>
2025-08-17 17:04:13 +0900

    [6749번] Next in line

---
+++ baekjoon/src/step00_by_ID/id_06000_06999/p06749/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.src.step00_by_ID.id_06000_06999.p06749;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        bw.append(String.valueOf(B + (B - A)));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
