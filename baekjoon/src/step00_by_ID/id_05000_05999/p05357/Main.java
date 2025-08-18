da9f05ecde6ae787220d681738446e263a0353bd
Luverduck <hyekko92@gmail.com>
2025-08-18 21:22:39 +0900

    [5357번] Dedupe

---
+++ baekjoon/src/step00_by_ID/id_05000_05999/p05357/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.src.step00_by_ID.id_05000_05999.p05357;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (N-- > 0) {
+            String s = br.readLine();
+            char prev = '*';
+            for (int i = 0; i < s.length(); ++i) {
+                char c = s.charAt(i);
+                if(c == prev) continue;
+                sb.append(c);
+                prev = c;
+            }
+            sb.append('\n');
+        }
+        bw.append(sb);
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
