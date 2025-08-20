e50c04fcbec72bedd2f7f614d82f3128d3af2a29
Luverduck <hyekko92@gmail.com>
2025-08-20 12:28:41 +0900

    [15873번] 공백 없는 A+B

---
+++ baekjoon/src/step00_by_ID/id_15000_15999/p15873/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.src.step00_by_ID.id_15000_15999.p15873;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        if(input.equals("1010"))
+            sb.append(20);
+        else if(input.contains("10")) {
+            input = input.replaceAll("10", "");
+            sb.append(10 + Integer.parseInt(input));
+        } else {
+            int number = Integer.parseInt(input);
+            sb.append(number / 10 + number % 10);
+        }
+        bw.append(sb);
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
