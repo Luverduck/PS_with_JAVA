2d64a22cbc1a89afe798198024a454bbdcf58cdc
Luverduck <hyekko92@gmail.com>
2025-08-29 00:30:59 +0900

    [11719번] 그대로 출력하기 2

---
+++ baekjoon/src/step00_by_ID/id_11000_11999/p11719/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.src.step00_by_ID.id_11000_11999.p11719;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input;
+        while(true) {
+            input = br.readLine();
+            if(input == null) break;
+            bw.append(input).append('\n');
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
