dee5c2838354c56a2d06e906d00f446f97c1fc33
Luverduck <hyekko92@gmail.com>
2025-08-29 18:21:02 +0900

    [2442번] 별 찍기 - 5

---
+++ baekjoon/src/step00_by_ID/id_02000_02999/p02442/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.src.step00_by_ID.id_02000_02999.p02442;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        for(int n = 0; n < N; ++n) {
+            bw.append(" ".repeat(N - n - 1));
+            bw.append("*".repeat(2 * n + 1));
+            bw.append('\n');
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
