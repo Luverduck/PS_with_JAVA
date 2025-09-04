501fc94807a0fcd743f56a62f57cb542c935d4dc
Luverduck <hyekko92@gmail.com>
2025-09-05 01:45:25 +0900

    [34052번] 체육은 수학과목 입니다 2

---
+++ baekjoon/src/step00_by_ID/id_34000_34999/P34052/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.src.step00_by_ID.id_34000_34999.P34052;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int sec = 0;
+        for (int i = 0; i < 4; ++i)
+            sec += Integer.parseInt(br.readLine());
+        bw.append(sec <= 1500 ? "Yes" : "No");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
