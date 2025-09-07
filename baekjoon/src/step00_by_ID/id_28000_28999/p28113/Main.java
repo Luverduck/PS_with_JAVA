8911f0e6626ccb0aedf18dd36aef36ff3d0640a8
Luverduck <hyekko92@gmail.com>
2025-09-07 18:20:15 +0900

    [28113번] 정보섬의 대중교통

---
+++ baekjoon/src/step00_by_ID/id_28000_28999/p28113/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.src.step00_by_ID.id_28000_28999.p28113;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int bus = A;
+        int subway = N + (B - N);
+        bw.append(bus == subway ? "Anything" : (bus < subway ? "Bus" : "Subway"));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
