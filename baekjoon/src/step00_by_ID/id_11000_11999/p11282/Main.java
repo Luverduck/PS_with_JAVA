1deb02757e682b08f85a760a16c67683425cc31f
Luverduck <hyekko92@gmail.com>
2025-09-17 20:57:28 +0900

    [11282번] 한글

---
+++ baekjoon/src/step00_by_ID/id_11000_11999/p11282/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.src.step00_by_ID.id_11000_11999.p11282;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()) - 1;
+        bw.append(String.valueOf(Character.toChars('가' + N)));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
