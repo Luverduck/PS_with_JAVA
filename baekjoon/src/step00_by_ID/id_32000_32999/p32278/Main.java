6a6827161fdc401bf3c25c499f61fae671bda513
Luverduck <hyekko92@gmail.com>
2025-09-23 09:10:41 +0900

    [32278번] 선택 가능성이 가장 높은 자료형

---
+++ baekjoon/src/step00_by_ID/id_32000_32999/p32278/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.src.step00_by_ID.id_32000_32999.p32278;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long input = Long.parseLong(br.readLine());
+        if(input <= Short.MAX_VALUE && input >= Short.MIN_VALUE) bw.append("short");
+        else if(input <= Integer.MAX_VALUE && input >= Integer.MIN_VALUE) bw.append("int");
+        else bw.append("long long");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
