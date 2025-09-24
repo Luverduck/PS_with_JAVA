b67c5a24ef6bff08eab3d85ad8fad4553e5d0efc
Luverduck <hyekko92@gmail.com>
2025-09-24 23:08:12 +0900

    [34446번] E-Days Ore Cart Pull

---
+++ baekjoon/src/step00_by_ID/id_34000_34999/p34446/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.src.step00_by_ID.id_34000_34999.p34446;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int M = Integer.parseInt(br.readLine());
+        bw.append(String.valueOf(2 * M));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
