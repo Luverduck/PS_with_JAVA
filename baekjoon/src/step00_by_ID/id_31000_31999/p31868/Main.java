97856f89be5352092a0ade7a9a2e3448f14d0279
Luverduck <hyekko92@gmail.com>
2025-10-03 13:03:53 +0900

    [31868번] 수박 게임

---
+++ baekjoon/src/step00_by_ID/id_31000_31999/p31868/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.src.step00_by_ID.id_31000_31999.p31868;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int res = K / (1 << (N - 1));
+        bw.append(String.valueOf(res));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
