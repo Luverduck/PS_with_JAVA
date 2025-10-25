83d02aebd547e45fdaaba1f02ef7cf16325ba92a
Luverduck <hyekko92@gmail.com>
2025-10-25 23:58:19 +0900

    [31822번] 재수강

---
+++ baekjoon/src/step00_by_ID/id_31000_31999/p31822/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.src.step00_by_ID.id_31000_31999.p31822;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String targetCode = br.readLine().substring(0, 5);
+        int N = Integer.parseInt(br.readLine());
+        int res = 0;
+        while (N-- > 0) {
+            String inputCode = br.readLine().substring(0, 5);
+            if (targetCode.equals(inputCode)) res++;
+        }
+        bw.append(String.valueOf(res));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
