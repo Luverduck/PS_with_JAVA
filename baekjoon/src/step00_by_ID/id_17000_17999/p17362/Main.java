35c8012f6b6b00338ac8595d855696e937d142f3
Luverduck <hyekko92@gmail.com>
2025-10-30 20:54:11 +0900

    [17362번] 수학은 체육과목 입니다 2

---
+++ baekjoon/src/step00_by_ID/id_17000_17999/p17362/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.src.step00_by_ID.id_17000_17999.p17362;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] ans = { 1, 2, 3, 4, 5, 4, 3, 2 };
+        int N = Integer.parseInt(br.readLine());
+        bw.append(String.valueOf(ans[(N - 1) % 8]));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
