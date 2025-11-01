78d6481eb08ab22a27c8f5d577877988209a678a
Luverduck <hyekko92@gmail.com>
2025-11-01 12:37:02 +0900

    [25628번] 햄버거 만들기

---
+++ baekjoon/src/step00_by_ID/id_25000_25999/p25628/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.src.step00_by_ID.id_25000_25999.p25628;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        bw.append(String.valueOf(Math.min(A / 2, B)));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
