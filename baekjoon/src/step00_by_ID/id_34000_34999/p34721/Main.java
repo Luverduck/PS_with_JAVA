445890ee7b8ee67bdade65c3914a7d55bd108f74
Luverduck <hyekko92@gmail.com>
2025-11-10 21:10:43 +0900

    [34721번] 역사를 걸으면 동국이 보이고

---
+++ baekjoon/src/step00_by_ID/id_34000_34999/p34721/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.src.step00_by_ID.id_34000_34999.p34721;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        bw.append("I love DGU\n".repeat(N));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
