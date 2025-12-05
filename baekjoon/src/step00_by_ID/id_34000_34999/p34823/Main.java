7b90d4cafcfd40202a19ed07f6964a0662e3f7e6
Luverduck <hyekko92@gmail.com>
2025-12-05 19:59:25 +0900

    [34823번] YCPC 점수

---
+++ baekjoon/src/step00_by_ID/id_34000_34999/p34823/Main.java
@@ -0,0 +1,22 @@
+package step00_by_ID.id_34000_34999.p34823;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("fileio/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int Y = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        bw.append(String.valueOf(Math.min(Math.min(Y, P), C / 2)));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
