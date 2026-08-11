7a144685a69f91834463d84920b94c7f0c35edba
Luverduck <hyekko92@gmail.com>
2026-08-11 19:42:59 +0900

    메인 클래스 작성

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        /*
+            로직 작성
+        */
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
