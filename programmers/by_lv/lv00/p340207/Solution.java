8eec759412abc0fd1aa9b044905858bd82eff0c7
Luverduck <hyekko92@gmail.com>
2026-08-11 20:43:23 +0900

    [PCCE 기출문제] 1번 / 문자 출력

---
+++ programmers/by_lv/lv00/p340207/Solution.java
@@ -0,0 +1,16 @@
+package programmers.by_lv.lv00.p340207;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * [PCCE 기출문제] 1번 / 문자 출력
+ * https://school.programmers.co.kr/learn/courses/30/lessons/340207
+ */
+
+public class Solution {
+    static void main() {
+        String message = "Let's go!";
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
