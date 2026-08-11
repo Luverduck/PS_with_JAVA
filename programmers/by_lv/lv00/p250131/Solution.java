28f4bbd9a4bdb4912a2be40bb94ef4d32b7348bb
Luverduck <hyekko92@gmail.com>
2026-08-11 22:53:36 +0900

    [PCCE 기출문제] 3번 / 나이 계산

---
+++ programmers/by_lv/lv00/p250131/Solution.java
@@ -0,0 +1,28 @@
+package programmers.by_lv.lv00.p250131;
+
+
+import java.util.Scanner;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * [PCCE 기출문제] 3번 / 나이 계산
+ * https://school.programmers.co.kr/learn/courses/30/lessons/250131
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        String age_type = sc.next();
+        int answer = 0;
+
+        if (age_type.equals("Korea")) {
+            answer = 2030 - year + 1;
+        } else if (age_type.equals("Year")) {
+            answer = 2030 - year;
+        }
+
+        System.out.println(answer);
+    }
+}
