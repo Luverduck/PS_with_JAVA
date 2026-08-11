e3f68a5f8076af4afdfb764458d9c97f72374f21
Luverduck <hyekko92@gmail.com>
2026-08-11 22:45:37 +0900

    [PCCE 기출문제] 1번 / 출력

---
+++ programmers/by_lv/lv00/p250133/Solution.java
@@ -0,0 +1,20 @@
+package programmers.by_lv.lv00.p250133;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * [PCCE 기출문제] 1번 / 출력
+ * https://school.programmers.co.kr/learn/courses/30/lessons/250133
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
