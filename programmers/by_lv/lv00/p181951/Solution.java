1a615c5f901febe6f7667d88c4429ce679a1ee1b
Luverduck <hyekko92@gmail.com>
2026-08-12 18:39:19 +0900

    a와 b 출력하기

---
+++ programmers/by_lv/lv00/p181951/Solution.java
@@ -0,0 +1,21 @@
+package programmers.by_lv.lv00.p181951;
+
+import java.util.Scanner;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * a와 b 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181951
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
