914549ced100ba94147c82cfd3e4021d2db41843
Luverduck <hyekko92@gmail.com>
2026-08-12 19:04:32 +0900

    문자열 붙여서 출력하기

---
+++ programmers/by_lv/lv00/p181946/Solution.java
@@ -0,0 +1,19 @@
+package programmers.by_lv.lv00.p181946;
+
+import java.util.Scanner;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 문자열 붙여서 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181946
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a.concat(b));
+    }
+}
