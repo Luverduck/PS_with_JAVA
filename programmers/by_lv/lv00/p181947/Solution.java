ff91f543467cc1f6f0c1a6b58aa539f2930921d1
Luverduck <hyekko92@gmail.com>
2026-08-12 19:02:48 +0900

    덧셈식 출력하기

---
+++ programmers/by_lv/lv00/p181947/Solution.java
@@ -0,0 +1,20 @@
+package programmers.by_lv.lv00.p181947;
+
+import java.util.Scanner;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 덧셈식 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181947
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
