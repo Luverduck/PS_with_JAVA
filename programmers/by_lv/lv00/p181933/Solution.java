d04f4d8f26e75fa5c9b7de9db4fc2e2da2cc95ac
Luverduck <hyekko92@gmail.com>
2026-08-13 15:49:07 +0900

    flag에 따라 다른 값 반환하기

--- programmers/by_lv/lv00/p181933/Solution.java
+++ programmers/by_lv/lv00/p181933/Solution.java
@@ -4,7 +4,7 @@ package programmers.by_lv.lv00.p181933;
  * 코딩테스트 연습
  * 코딩 기초 트레이닝
  * flag에 따라 다른 값 반환하기
- * https://school.programmers.co.kr/learn/courses/30/lessons/181934
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181933
  */
 
 class Solution {
