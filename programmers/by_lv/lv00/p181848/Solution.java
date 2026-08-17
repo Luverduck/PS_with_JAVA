a294191259167cb8360c96159cdd8c5115e35f51
Luverduck <hyekko92@gmail.com>
2026-08-18 01:47:05 +0900

    문자열을 정수로 변환하기

---
+++ programmers/by_lv/lv00/p181848/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p181848;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 문자열을 정수로 변환하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181848
+ */
+
+class Solution {
+    public int solution(String n_str) {
+        return Integer.parseInt(n_str);
+    }
+}
