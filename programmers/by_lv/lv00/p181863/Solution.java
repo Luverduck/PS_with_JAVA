b3bc990e34a9a12b061bb5094a2b7a1647d40d06
Luverduck <hyekko92@gmail.com>
2026-08-17 18:28:41 +0900

    rny_string

---
+++ programmers/by_lv/lv00/p181863/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p181863;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * rny_string
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181863
+ */
+
+class Solution {
+    public String solution(String rny_string) {
+        return rny_string.replace("m", "rn");
+    }
+}
