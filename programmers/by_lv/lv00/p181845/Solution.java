d447f799a433d051201d7cd6e8f892326a018878
Luverduck <hyekko92@gmail.com>
2026-08-18 02:05:45 +0900

    문자열로 변환

---
+++ programmers/by_lv/lv00/p181845/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p181845;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 문자열로 변환
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181845
+ */
+
+class Solution {
+    public String solution(int n) {
+        return String.valueOf(n);
+    }
+}
