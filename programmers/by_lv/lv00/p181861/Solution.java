db00b7a862ef8050df6e25e69ed5a35a9c4c98ab
Luverduck <hyekko92@gmail.com>
2026-08-17 19:01:41 +0900

    배열의 원소만큼 추가하기

---
+++ programmers/by_lv/lv00/p181861/Solution.java
@@ -0,0 +1,27 @@
+package programmers.by_lv.lv00.p181861;
+
+import java.util.Arrays;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 배열의 원소만큼 추가하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181861
+ */
+
+class Solution {
+    static public int[] solution(int[] arr) {
+        int length = 0;
+        for (int n : arr) length += n;
+        int[] res = new int[length];
+        int idx = 0;
+        for (int n : arr) {
+            Arrays.fill(res, idx, idx + n, n);
+            idx += n;
+        }
+        return res;
+    }
+    /*static void main() {
+        solution(new int[] {5, 1, 4});
+    }*/
+}
