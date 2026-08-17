7ef7974bd229d2720a958e8b597b03fd01f7a354
Luverduck <hyekko92@gmail.com>
2026-08-18 03:12:54 +0900

    커피 심부름

--- programmers/by_lv/lv00/p181837/Solution.java
+++ programmers/by_lv/lv00/p181837/Solution.java
@@ -9,11 +9,11 @@ package programmers.by_lv.lv00.p181837;
 
 class Solution {
     public int solution(String[] order) {
-        int[] cnt = new int[2];
+        int sum = 0;
         for (String s : order) {
-            if (s.contains("americano") || s.equals("anything")) cnt[0]++;
-            else if (s.contains("cafelatte")) cnt[1]++;
+            if (s.contains("cafelatte")) sum += 5000;
+            else sum += 4500;
         }
-        return 4500 * cnt[0] + 5000 * cnt[1];
+        return sum;
     }
 }
