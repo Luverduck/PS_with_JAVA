f30eecd1201958ffd3217aa37aeb1079b8f5b184
Luverduck <hyekko92@gmail.com>
2026-08-17 22:18:06 +0900

    정수 부분

---
+++ programmers/by_lv/lv00/p181850/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p181850;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 정수 부분
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181850
+ */
+
+class Solution {
+    public int solution(double flo) {
+        return (int) flo;
+    }
+}
