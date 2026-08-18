1d0568a18f8eb980b48b41ce7448e4c1948c20a2
Luverduck <hyekko92@gmail.com>
2026-08-18 20:46:37 +0900

    종이 자르기

---
+++ programmers/by_lv/lv00/p120922/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p120922;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 종이 자르기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120922
+ */
+
+class Solution {
+    public int solution(int M, int N) {
+        return M * N - 1;
+    }
+}
