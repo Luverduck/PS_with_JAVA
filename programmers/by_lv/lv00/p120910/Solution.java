f5f9b05f39752705a0bc895c32709b7d239b1702
Luverduck <hyekko92@gmail.com>
2026-08-19 01:19:33 +0900

    세균 증식

---
+++ programmers/by_lv/lv00/p120910/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p120910;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 세균 증식
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120910
+ */
+
+class Solution {
+    public int solution(int n, int t) {
+        return n << t;
+    }
+}
