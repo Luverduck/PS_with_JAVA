ffd3c29705b96e0f6f83ac61265b786b47a503c8
Luverduck <hyekko92@gmail.com>
2026-08-19 02:34:11 +0900

    편지

---
+++ programmers/by_lv/lv00/p120898/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p120898;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 편지
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120898
+ */
+
+class Solution {
+    public int solution(String message) {
+        return message.length() * 2;
+    }
+}
