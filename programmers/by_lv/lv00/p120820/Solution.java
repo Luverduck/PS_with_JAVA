3ce58bfbee3a83998966a01bd812dd116d63ab59
Luverduck <hyekko92@gmail.com>
2026-08-25 01:38:01 +0900

    나이 출력

---
+++ programmers/by_lv/lv00/p120820/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p120820;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 나이 출력
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120820
+ */
+
+class Solution {
+    public int solution(int age) {
+        return 2022 - age + 1;
+    }
+}
