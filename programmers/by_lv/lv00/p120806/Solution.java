d63d4fb53dc5a75677671a4448504f3c4cee7ee8
Luverduck <hyekko92@gmail.com>
2026-08-26 21:03:59 +0900

    두 수의 나눗셈

---
+++ programmers/by_lv/lv00/p120806/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p120806;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 두 수의 나눗셈
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120806
+ */
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return 1000 * num1 / num2;
+    }
+}
