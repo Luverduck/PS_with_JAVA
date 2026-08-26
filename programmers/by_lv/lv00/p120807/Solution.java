35e5ee6e70463fe8ecb85d65e1a2550cad012384
Luverduck <hyekko92@gmail.com>
2026-08-26 21:00:08 +0900

    숫자 비교하기

---
+++ programmers/by_lv/lv00/p120807/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p120807;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 숫자 비교하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120807
+ */
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 == num2 ? 1 : -1;
+    }
+}
