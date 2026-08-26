837041d54ef2ebc35c62d41b9d858ade198d0b73
Luverduck <hyekko92@gmail.com>
2026-08-26 21:06:00 +0900

    두 수의 합 구하기

---
+++ programmers/by_lv/lv00/p120802/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p120802;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 두 수의 합 구하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120802
+ */
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
