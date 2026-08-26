bdf16ca15db2f9f588c8a564845826df1bf44438
Luverduck <hyekko92@gmail.com>
2026-08-26 20:50:04 +0900

    나머지 구하기

---
+++ programmers/by_lv/lv00/p120810/Solution.java
@@ -0,0 +1,14 @@
+package programmers.by_lv.lv00.p120810;
+
+/**
+ * 코딩테스트 연습
+ * 코딩 기초 트레이닝
+ * 나머지 구하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120810
+ */
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 % num2;
+    }
+}
